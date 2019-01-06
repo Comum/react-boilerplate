c85271d1dae7ae03191e1dedfc7682b337191b52
Comum <miguel.rib.20@gmail.com>
2019-01-06 23:59:16 +0000

    Add asynchronous dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
 
+const Warning = React.lazy(() => import ('./warning'))
+
 class App extends React.Component {
     state = {
         count: 0
@@ -25,6 +27,11 @@ class App extends React.Component {
                 </h2>
                 <button onClick={this.increment}>+</button>
                 <button onClick={this.decrement}>-</button>
+                {count > 10 ?
+                    <React.Suspense fallback={null}>
+                        <Warning />
+                    </React.Suspense>
+                    : null}
             </div>
         )
     }
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -30,7 +30,8 @@ module.exports = {
                         '@babel/preset-react'],
                     plugins: [
                         'react-hot-loader/babel',
-                        '@babel/plugin-proposal-class-properties'
+                        '@babel/plugin-proposal-class-properties',
+                        '@babel/plugin-syntax-dynamic-import'
                     ]
                 }
             },
