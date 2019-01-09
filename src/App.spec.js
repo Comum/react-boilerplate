65a5f9f2fb8a9b975c57638f58e51908aee20ed3
Comum <miguel.rib.20@gmail.com>
2019-01-09 19:34:13 +0000

    Config project to be able to use jest

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,11 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+import App from './App'
+
 describe('App', () => {
-    it('Runs and passes', () => {
-        expect(true).toBe(true)
+    it('Renders without error', () => {
+        render(<App />)
     })
 })
