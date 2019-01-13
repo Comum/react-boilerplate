7bd753ffc046320a4f8c86a6f893bdb7f153a57a
Comum <miguel.rib.20@gmail.com>
2019-01-13 18:18:45 +0000

    Add an error boundary

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import '@babel/polyfill'
 
 import App from './App'
 import './styles.css'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <DefaultErrorBoundary>
+            <App />
+        </DefaultErrorBoundary>
     </React.StrictMode>,
     document.getElementById('app')
 )
