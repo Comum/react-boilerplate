d733beb888a382bf73aeafd3ffcd27d57cd2e542
Comum <miguel.rib.20@gmail.com>
2019-01-13 18:25:39 +0000

    Add acessibility checking with react-axe

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import App from './App'
 import './styles.css'
 import DefaultErrorBoundary from './DefaultErrorBoundary'
 
+if (process.env.NODE_ENV === 'development') {
+    const axe = require('react-axe')
+
+    axe(React, ReactDOM, 1000)
+}
+
 ReactDOM.render(
     <React.StrictMode>
         <DefaultErrorBoundary>
